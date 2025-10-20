13a2615a0011ea79f902701893ea98a9654ea730
Yair Rodriguez <yair.rodriguez@wolterskluwer.com>
2025-10-20 17:21:54 -0600

    Refactor API and introduce CartController
    - Added `YairUtilities.ApiUtilities` package for middleware support.
    - Removed `WeatherForecastController` and `WeatherForecast` model.
    - Added global exception handling middleware in `Program.cs`.
    - Introduced `CartController` as a placeholder for cart management.

---
+++ CartService.API/Controllers/CartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YairUtilities.ApiUtilities.Classes.Common;
+
+namespace CartService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ApiControllerBase
+    {
+    }
+}
--- CartService.API/Program.cs
+++ CartService.API/Program.cs
@@ -1,3 +1,5 @@
+using YairUtilities.ApiUtilities.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Using custom middleware
+app.UseGlobalExceptionHandling();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
